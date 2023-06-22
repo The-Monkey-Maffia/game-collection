b6ba210086d2e416b92e72a5201763cfcf2319b0
quickrick2 <rick.kampstra2002@gmail.com>
2023-06-22 10:54:26 +0200

    Update GameManager.cs

--- Assets/TowerDefense/Scripts/GameManager.cs
+++ Assets/TowerDefense/Scripts/GameManager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int[] towers = {0, 1};
+    public int[] units = {0, 1};
+    public int health = 100;
+
+    public int[] availableTowers = {};
+    public int[] availableUnits = {};
+    public int nextTower = 0;
+    public int nextUnit = 0;
+
     // Start is called before the first frame update
     void Start()
     {
