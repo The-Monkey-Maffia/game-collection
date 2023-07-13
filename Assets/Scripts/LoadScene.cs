84958a4ccaa0cb672ed3ddd70f75766cf5a6bc68
quickrick2 <rick.kampstra2002@gmail.com>
2023-07-13 11:37:02 +0200

    Added buttons to manage scene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public int sceneNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+}
